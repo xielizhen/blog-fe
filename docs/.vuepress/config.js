35f965749441bd54288828aa8a9ec955b0be62e0
xielizhen <xielizhen@baixing.com>
2021-07-19 13:57:33 +0800

    update:信息

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,32 @@
 module.exports = {
   base: '/blog',
-  title: 'hello vuepress',
-  description: 'just playing around',
+  title: '谢丽珍的博客',
+  description: '',
   head: [
     ['link', { rel: 'icon', href: '/logo.png' }]
   ],
+  themeConfig: {
+    nav: [ 
+      {text: '主页', link: '/'},
+      {text: '文档', link: '/guide/'},
+      {text: '交流', link: 'zyqq/wheel'},
+    ],
+    sidebar: [
+      {
+        title: 'Webpack',
+        path: '/webpack',
+        collapsable: false,
+        sidebarDepth: 1,
+        children: [
+          {
+            title: '手把手撸loader',
+            path: '/webpack/loader',
+            sidebarDepth: 2
+          }
+        ]
+      }
+    ]
+  },
   configureWebpack: {
     resolve: {
       alias: {
