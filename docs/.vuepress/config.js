f16813903a886ee995864d8cbbfb96a93f832cbf
xielizhen <15000734841@163.com>
2021-08-23 11:37:27 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,14 +2,11 @@ module.exports = {
   base: '/blog',
   title: '谢丽珍的博客',
   description: '',
-  head: [
-    ['link', { rel: 'icon', href: '/logo.png' }]
-  ],
   themeConfig: {
     nav: [ 
       {text: '主页', link: '/'},
       {text: '文档', link: '/guide/'},
-      {text: '交流', link: 'zyqq/wheel'},
+      {text: '交流', link: '/zyqq/wheel'},
     ],
     sidebar: [
       {
